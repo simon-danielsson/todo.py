49ce0ec6b2b78f4fa02612e3576f927e44a94232
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-05-26 06:18:51 +0200

    feat(todo.py): first working version

--- src/todo.py
+++ src/todo.py
@@ -8,7 +8,20 @@ from typing import Generator
 
 # constants -------------------------------------------------------------------
 
-BLACKLIST: list[str] = ["LICENSE", ".git", "README"]
+ANSI_GRN: str = "\033[1;32m"
+ANSI_BLU: str = "\033[1;34m"
+ANSI_RST: str = "\033[0m"
+
+BLACKLIST: list[str] = [
+        "LICENSE",
+        "todo.py",
+        "license",
+        ".git",
+        "README",
+        ".sample",
+        ".log",
+        "git",
+        ]
 
 # program ---------------------------------------------------------------------
 
@@ -21,62 +34,82 @@ class Todo:
 
 def collect_files_in_cd() -> list[Path]:
     CD = Path.cwd()
-    return [path for path in CD.rglob("*") if path.is_file()]
+    return [path for path in CD.rglob("*") if not path.is_dir()]
 
-# git blame --line-porcelain -L 42,42 path/to/file | grep '^author-time'
-def get_time_created(f: Path, l: int) -> datetime:
-    process = subprocess.run(
-            [
-                "git",
-                "blame",
-                "--line-porcelain",
-                "-L",
-                f"{l},{l}",
-                f.absolute(),
-                "|",
-                "grep",
-                "'^author-time'",
-                ],
-            capture_output=True,
-            text=True,
-            )
-    return datetime.fromtimestamp(float(process.stdout.strip()))
+def get_time_created(f: Path, l: int) -> datetime | None:
+    if l == 0:
+        l += 1
+    try:
+        process = subprocess.run(
+                [
+                    "git",
+                    "blame",
+                    "--line-porcelain",
+                    "-L",
+                    f"{l},{l}",
+                    str(f.absolute()),
+                    ],
+                capture_output=True,
+                text=True,
+                check=True,
+                )
+    except subprocess.CalledProcessError as e:
+        print(
+                f"{e.stderr}".strip(),
+                f'\nCould not process TODO in "{f.name}", skipping file...\n',
+                )
+        return None
+    for line in process.stdout.splitlines():
+        if line.startswith("author-time "):
+            timestamp = int(line.split()[1])
+            return datetime.fromtimestamp(timestamp)
 
 def scan_file(f: Path) -> Generator[Todo]:
     l = 0
-    it = f.open()
+    it = f.open(errors="ignore", encoding="utf-8")
     lines = it.readlines()
-    while True:
-        try:
-            if "TODO" in lines[l]:
-                td: Todo = Todo(
-                        line=l, content_lines=[], path=f, created=get_time_created(f=f, l=l)
-                        )
-                td.content_lines.append(lines[l])
-                l += 1
-                while not lines[l].isspace():
-                    try:
-                        td.content_lines.append(lines[l])
-                        l += 1
-                    except:
-                        break
-                yield td
-            l += 1
-        except:
-            it.close()
-            return
+    for i, l in enumerate(lines):
+        if "TODO:" in l:
+            # print(f"found: {f.name}")
+            created = get_time_created(f=f, l=i)
+            if created == None:
+                return
+            td: Todo = Todo(line=i, content_lines=[], path=f, created=created)
+            cl = i
+            while cl < len(lines):
+                if lines[cl].strip() != "":
+                    td.content_lines.append(lines[cl].strip("\n"))
+                    cl += 1
+                    continue
+                break
+            yield td
+    it.close()
 
-def main():
+def print_todo(t: list[Todo]) -> None:
+    if len(t) < 1:
+        print("No TODO items were found.")
+        return
+    print(f"{ANSI_GRN}{len(t)} TODO items were found!{ANSI_RST}")
+
+    for i in t:
+        print(
+                f"\n{ANSI_BLU}File:{ANSI_RST} {i.path.name}:{i.line}"
+                f"\n{ANSI_BLU}Date:{ANSI_RST} {i.created.date()} {i.created.time()}"
+                f"\n╭────────────────────────"
+                )
+        for l in i.content_lines:
+            print(f"│ {l}")
+
+def main() -> None:
     files = collect_files_in_cd()
     todo_items: list[Todo] = []
     for f in files:
         if not any(n in f.name for n in BLACKLIST):
-            try:
-                todo_items += list(scan_file(f))
-            except:
-                break
-        else:
-            print(f"ignored: {f.name}")
+            todo_items += list(scan_file(f))
+        # else:
+        #     print(f"ignored: {f.name}")
+
+    print_todo(todo_items)
 
 if __name__ == "__main__":
     main()
